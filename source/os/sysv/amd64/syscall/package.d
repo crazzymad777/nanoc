169f04bf4c2bfd2b9150ab800c6618bade8e39cd
Yury Moskov <aerobus.778@gmail.com>
2024-11-17 00:40:32 +0300

    init nanoc with sysV/amd64 syscalls

---
+++ source/os/sysv/amd64/syscall/package.d
@@ -0,0 +1,22 @@
+module nanoc.os.sysv.amd64.syscall;
+
+version (DigitalMars) {
+   public import nanoc.os.sysv.amd64.syscall.dmd: raw_syscall;
+}
+
+version (LDC) {
+    public import nanoc.os.sysv.amd64.syscall.ldc: raw_syscall;
+}
+
+version (GNU) {
+    public import nanoc.os.sysv.amd64.syscall.gdc: raw_syscall;
+}
+
+long syscall(T...)(T args)
+{
+    static if (args.length == 0)
+    {
+        static assert(false, "syscall: provided no arguments. At least syscall number required.");
+    }
+    return raw_syscall(args);
+}
