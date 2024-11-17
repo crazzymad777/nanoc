05ec997091e368d9d4b79a1fee9c004cb00b5a8c
Yury Moskov <aerobus.778@gmail.com>
2024-11-17 06:00:30 +0300

    update example

--- source/app.d
+++ source/app.d
@@ -11,15 +11,29 @@ extern (C) int main() {
     //     puts("Parent\n".ptr);
     // }
     // exit(0);
-    import nanoc.std.string;
+
+    // import nanoc.std.string;
+    // import nanoc.std.stdlib;
+    // import nanoc.std.stdio;
+    // int fd = open("my.txt", O_WRONLY | O_CREAT, 0);
+    // if (fd < 0) {
+    //     puts("Cann't open\n");
+    //     exit(-25);
+    // }
+    // string greeting = "Привет, Юра!\n";
+    // write(fd, greeting, strlen(greeting.ptr));
+    // close(fd);
+    // //return 0;
+    // exit(0);
+    import nanoc.std.stdlib;
     import nanoc.std.stdio;
-    int fd = open("my.txt", O_WRONLY | O_CREAT, 0);
-    if (fd < 0) {
-        puts("Cann't open\n");
-        return -1;
+    void *p = _malloc(10000);
+    if (!p)
+    {
+        puts("Cann't allocate\n");
+        exit(-25);
     }
-    string greeting = "Привет, Юра!\n";
-    write(fd, greeting, strlen(greeting.ptr));
-    close(fd);
-    return 0;
+    puts("Prepare to free\n");
+    free(p);
+    exit(0);
 }
