61e45c7155476ccabd3a4d0aa2221bbe4f8836a9
Yury Moskov <aerobus.778@gmail.com>
2024-11-17 03:01:32 +0300

    add open/close

--- source/os/sysv/amd64/syscall/linux.d
+++ source/os/sysv/amd64/syscall/linux.d
@@ -3,6 +3,7 @@ module nanoc.os.sysv.amd64.linux;
 enum SYS_read = 0;
 enum SYS_write = 1;
 enum SYS_open = 2;
+enum SYS_close = 3;
 enum SYS_fork = 57;
 enum SYS_exit = 60;
 
--- source/std/stdio.d
+++ source/std/stdio.d
@@ -1,5 +1,6 @@
 module nanoc.std.stdio;
 
+alias mode_t = int;
 enum EOF = -1;
 
 extern(C) int puts(const char *str)
@@ -13,6 +14,12 @@ extern(C) int puts(const char *str)
     return EOF;
 }
 
+extern(C) int open(const char *pathname, int flags, mode_t mode)
+{
+    import nanoc.os: syscall, SYS_open;
+    return cast(int) syscall(SYS_open, cast(void*) pathname, flags, mode);
+}
+
 extern(C) size_t write(int fd, const void[] buf, size_t count)
 {
     import nanoc.os: syscall, SYS_write;
@@ -23,4 +30,10 @@ extern(C) size_t read(int fd, void[] buf, size_t count)
 {
     import nanoc.os: syscall, SYS_read;
     return syscall(SYS_read, fd, cast(void*) buf, count);
+}
+
+extern(C) int close(int fd)
+{
+    import nanoc.os: syscall, SYS_close;
+    return cast(int) syscall(SYS_close, fd);
 }
