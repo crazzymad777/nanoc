8cae52c59911fc2567507d3410bc6af989367ebf
Yury Moskov <aerobus.778@gmail.com>
2024-11-20 21:46:20 +0300

    implement for ASCII characters toupper tolower iscntrl isblank isspace isupper islower isalpha isdigit isxdigit isalnum ispunct isgraph isprint

--- source/std/ctype.d
+++ source/std/ctype.d
@@ -1,24 +1,126 @@
 module nanoc.std.ctype;
 
-extern(C) int toupper(int wc)
+unittest
 {
-    auto toUpper(C)(C c)
-    if (is(C : dchar))
+    assert(toupper('a') == 'A');
+    assert(tolower('G') == 'g');
+    for (int i = 0; i < 255; i++)
     {
-        import std.traits : OriginalType;
-
-        static if (!__traits(isScalar, C))
-            alias R = dchar;
-        else static if (is(immutable OriginalType!C == immutable OC, OC))
-            alias R = OC;
-
-        return isLower(c) ? cast(R)(cast(R) c - ('a' - 'A')) : cast(R) c;
+        assert(iscntrl(i) == !isprint(i));
     }
+}
 
-    bool isLower(dchar c) @safe pure nothrow @nogc
+// for ASCII
+extern(C) int toupper(int x)
+{
+    return islower(x) ? x - ('a' - 'A') : x;
+}
+
+extern (C) int tolower(int x)
+{
+    return isupper(x) ? x - ('A' - 'a') : x;
+}
+
+extern (C) int iscntrl(int x) pure
+{
+    if (x < 0x20 || x == 0x7F)
     {
-        return c >= 'a' && c <= 'z';
+        return true;
     }
+    return false;
+}
 
-    return toUpper(wc);
+extern (C) int isblank(int x) pure
+{
+    if (x == 0x09 || x == 0x20)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isspace(int x) pure
+{
+    if (x >= 0x09 && x <= 0x0D || x == 0x20)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isupper(int x) pure
+{
+    if (x >= 0x41 && x <= 0x5A)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int islower(int x) pure
+{
+    if (x >= 0x61 && x <= 0x7A)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isalpha(int x) pure
+{
+    return isupper(x) || islower(x);
+}
+
+extern (C) int isdigit(int x) pure
+{
+    if (x >= '0' && x <= '9')
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isalnum(int x) pure
+{
+    return isalpha(x) || isdigit(x);
+}
+
+extern (C) int isxdigit(int x) pure
+{
+    if (isdigit(x))
+    {
+        return true;
+    }
+    if (x >= 'A' && x <= 'F')
+    {
+        return true;
+    }
+    if (x >= 'a' && x <= 'f')
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isprint(int x) pure
+{
+    if (x >= 0x20 && x != 0x7F)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int isgraph(int x)
+{
+     if (x > 0x20 && x != 0x7F)
+    {
+        return true;
+    }
+    return false;
+}
+
+extern (C) int ispunct(int x)
+{
+    return isgraph(x) && !isalnum(x);
 }
