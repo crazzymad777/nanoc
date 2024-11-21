ad2030177e360dd8f2d5129b914c6a6fff663094
Yury Moskov <aerobus.778@gmail.com>
2024-11-21 13:15:41 +0300

    update entry.d

--- source/entry.d
+++ source/entry.d
@@ -2,16 +2,32 @@ module nanoc.entry;
 
 extern(C)
 {
-    void __nanoc_init()
+    void __nanoc_init(int argc, char** argv, char **envp)
     {
+        const AUX_CNT = 38;
+        ulong[AUX_CNT] _aux;
 
+        int i;
+        for (i = 0; envp[i] !is null; i++) { }
+        ulong* auxv = cast(ulong*)(envp) + i + 1;
+
+        for (i = 0; auxv[i]; i += 2)
+        {
+            if (auxv[i] < AUX_CNT)
+            {
+                _aux[auxv[i]] = auxv[i+1];
+            }
+        }
+
+        import nanoc.thread: tls_init;
+        tls_init(_aux);
     }
 
-    int main(int argc, char** argv);
+    int main(int argc, char** argv, char** envp);
 
-    int __nanoc_main(int argc, char** argv)
+    int __nanoc_main(int argc, char** argv, char **envp)
     {
-        return main(argc, argv);
+        return main(argc, argv, envp);
     }
 
     import core.attribute: weak;
