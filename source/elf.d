20ffb7f9dd040e617a1541ef97e8baa13d79ba25
Yury Moskov <aerobus.778@gmail.com>
2024-11-21 18:18:46 +0300

    DISABLE_ELF

--- source/elf.d
+++ source/elf.d
@@ -18,13 +18,99 @@
 
 module nanoc.elf;
 
-import core.stdc.config;
 
-extern (C):
+version (DISABLE_ELF)
+{
+    /* Legal values for a_type (entry type).  */
+
+    enum AT_NULL = 0; /* End of vector */
+    enum AT_IGNORE = 1; /* Entry should be ignored */
+    enum AT_EXECFD = 2; /* File descriptor of program */
+    enum AT_PHDR = 3; /* Program headers for program */
+    enum AT_PHENT = 4; /* Size of program header entry */
+    enum AT_PHNUM = 5; /* Number of program headers */
+    enum AT_PAGESZ = 6; /* System page size */
+    enum AT_BASE = 7; /* Base address of interpreter */
+    enum AT_FLAGS = 8; /* Flags */
+    enum AT_ENTRY = 9; /* Entry point of program */
+    enum AT_NOTELF = 10; /* Program is not ELF */
+    enum AT_UID = 11; /* Real uid */
+    enum AT_EUID = 12; /* Effective uid */
+    enum AT_GID = 13; /* Real gid */
+    enum AT_EGID = 14; /* Effective gid */
+    enum AT_CLKTCK = 17; /* Frequency of times() */
+
+    /* Some more special a_type values describing the hardware.  */
+    enum AT_PLATFORM = 15; /* String identifying platform.  */
+    enum AT_HWCAP = 16; /* Machine-dependent hints about
+                        processor capabilities.  */
+
+    /* This entry gives some information about the FPU initialization
+    performed by the kernel.  */
+    enum AT_FPUCW = 18; /* Used FPU control word.  */
+
+    /* Cache block sizes.  */
+    enum AT_DCACHEBSIZE = 19; /* Data cache block size.  */
+    enum AT_ICACHEBSIZE = 20; /* Instruction cache block size.  */
+    enum AT_UCACHEBSIZE = 21; /* Unified cache block size.  */
+
+    /* A special ignored value for PPC, used by the kernel to control the
+    interpretation of the AUXV. Must be > 16.  */
+    enum AT_IGNOREPPC = 22; /* Entry should be ignored.  */
+
+    enum AT_SECURE = 23; /* Boolean, was exec setuid-like?  */
+
+    enum AT_BASE_PLATFORM = 24; /* String identifying real platforms.*/
+
+    enum AT_RANDOM = 25; /* Address of 16 random bytes.  */
+
+    enum AT_HWCAP2 = 26; /* More machine-dependent hints about
+                        processor capabilities.  */
+
+    enum AT_RSEQ_FEATURE_SIZE = 27; /* rseq supported feature size.  */
+    enum AT_RSEQ_ALIGN = 28; /* rseq allocation alignment.  */
+
+    /* More machine-dependent hints about processor capabilities.  */
+    enum AT_HWCAP3 = 29; /* extension of AT_HWCAP.  */
+    enum AT_HWCAP4 = 30; /* extension of AT_HWCAP.  */
+
+    enum AT_EXECFN = 31; /* Filename of executable.  */
+
+    /* Pointer to the global system page used for system calls and other
+    nice things.  */
+    enum AT_SYSINFO = 32;
+    enum AT_SYSINFO_EHDR = 33;
+
+    /* Shapes of the caches.  Bits 0-3 contains associativity; bits 4-7 contains
+    log2 of line size; mask those to get cache size.  */
+    enum AT_L1I_CACHESHAPE = 34;
+    enum AT_L1D_CACHESHAPE = 35;
+    enum AT_L2_CACHESHAPE = 36;
+    enum AT_L3_CACHESHAPE = 37;
+
+    /* Shapes of the caches, with more room to describe them.
+    *GEOMETRY are comprised of cache line size in bytes in the bottom 16 bits
+    and the cache associativity in the next 16 bits.  */
+    enum AT_L1I_CACHESIZE = 40;
+    enum AT_L1I_CACHEGEOMETRY = 41;
+    enum AT_L1D_CACHESIZE = 42;
+    enum AT_L1D_CACHEGEOMETRY = 43;
+    enum AT_L2_CACHESIZE = 44;
+    enum AT_L2_CACHEGEOMETRY = 45;
+    enum AT_L3_CACHESIZE = 46;
+    enum AT_L3_CACHEGEOMETRY = 47;
+
+    enum AT_MINSIGSTKSZ = 51; /* Stack needed for signal delivery  */
+}
+else
+{
 
 enum _ELF_H = 1;
 
 /* Standard ELF types.  */
+
+alias c_long = long;
+alias c_ulong = ulong;
 
 /* Type for a 16-bit quantity.  */
 alias Elf32_Half = ushort;
@@ -4557,3 +4643,4 @@ enum R_OR1K_TLS_DTPOFF = 33;
 enum R_OR1K_TLS_DTPMOD = 34;
 
 /* elf.h */
+}
