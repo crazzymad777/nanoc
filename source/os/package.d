0c1f5aee7821c26835075f1841d0ab4ea88b177d
Yury Moskov <aerobus.778@gmail.com>
2024-11-26 01:43:36 +0300

    portable-io: introduce struct Buffer

--- source/os/package.d
+++ source/os/package.d
@@ -10,21 +10,48 @@ version (X86_64)
     }
 }
 
-struct Buffer
+struct Buffer(bool nullable = false, bool autocount = false)
 {
-    const char* data;
-    size_t length;
-
-    void checkLength()
+    this(const char* data, size_t length)
     {
-        if (length == -1)
+        static if (nullable == false)
         {
-            import nanoc.std.string: strlen;
-            length = strlen(data);
+            assert(data !is null);
         }
+
+        static if (autocount)
+        {
+            if (length == -1)
+            {
+                static if (nullable)
+                {
+                    if (data is null)
+                    {
+                        length = 0;
+                        return;
+                    }
+                }
+                import nanoc.std.string: strlen;
+                length = strlen(data);
+            }
+        }
+
+        this.data = data;
+        this.len = length;
     }
+    const char* data;
+    size_t len;
 }
 
+// Nullable, Autocount = NullableString
+// Autocount = StringBuffer
+// Nullable = NullableBuffer
+// _ = MemoryChunk
+
+alias MemoryChunk = Buffer!(false, false);
+alias NullableBuffer = Buffer!(true, false);
+alias StringBuffer = Buffer!(false, true);
+alias NullableString = Buffer!(true, true);
 
 /// Terminate a process with given Status
 //noreturn exit(int status);
