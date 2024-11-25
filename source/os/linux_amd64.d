9eb23cd44c0b617f9d7df7b145c70ea3aa6f948a
Yury Moskov <aerobus.778@gmail.com>
2024-11-26 00:07:39 +0300

    portable: move sys/mman

--- source/os/linux_amd64.d
+++ source/os/linux_amd64.d
@@ -138,3 +138,21 @@ extern(C) int mkdir(const char* pathname, mode_t mode)
 {
     return cast(int) syscall(SYS_mkdir, pathname, mode);
 }
+
+alias off_t = long;
+enum PROT_READ = 1;
+enum PROT_WRITE = 2;
+enum MAP_SHARED = 0x0001;
+enum MAP_PRIVATE = 0x0002;
+enum MAP_ANONYMOUS = 0x0020;
+
+extern (C) void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset)
+{
+    return cast(void*) syscall(SYS_mmap, addr, length, prot, flags, fd, offset);
+}
+
+extern (C) int munmap(void* addr, size_t length)
+{
+    return cast(int) syscall(SYS_munmap, addr, length);
+}
+
--- source/sys/mman.d
+++ source/sys/mman.d
@@ -1,20 +1,3 @@
 module nanoc.sys.mman;
 
-alias off_t = long;
-enum PROT_READ = 1;
-enum PROT_WRITE = 2;
-enum MAP_SHARED = 0x0001;
-enum MAP_PRIVATE = 0x0002;
-enum MAP_ANONYMOUS = 0x0020;
-
-extern (C) void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset)
-{
-    import nanoc.os: syscall, SYS_mmap;
-    return cast(void*) syscall(SYS_mmap, addr, length, prot, flags, fd, offset);
-}
-
-extern (C) int munmap(void* addr, size_t length)
-{
-    import nanoc.os: syscall, SYS_munmap;
-    return cast(int) syscall(SYS_munmap, addr, length);
-}
+public import nanoc.os: mmap, munmap, off_t, PROT_READ, PROT_WRITE, MAP_SHARED, MAP_PRIVATE, MAP_ANONYMOUS;
