4806511e4b210958692fa6a72e2ab64b869d45d9
Yury Moskov <aerobus.778@gmail.com>
2024-11-30 19:11:50 +0300

    stdio: use package as entry, hide function in core.d, get rid of checkStdHandler

---
+++ source/std/stdio/file/core.d
@@ -0,0 +1,236 @@
+module nanoc.std.stdio.file.core;
+
+import nanoc.std.stdio.common;
+import nanoc.std.stdio.file;
+
+/// Internal struct for File
+struct FILE {
+    /// Define implementation of File template interface (template FileInterface(alias A))
+    enum Type {
+        OS = 1,
+        MEMORY_STREAM = 2,
+        DYNAMIC_MEMORY_STREAM = 3,
+        COOKIE = 4
+    }
+
+    /// Struct for memory streams
+    struct Mem {
+        /// pointer to buffer
+        void* data_ptr;
+        /// size of buffer
+        size_t size;
+        /// ftell
+        long offset;
+        /// indicates callee must free data_ptr
+        bool callee_free;
+
+        /// Only dynamic memory stream: for updating user point
+        void** dynamic_data;
+        /// Only dynamic memory stream: for updating user size
+        size_t* dynamic_size;
+    }
+
+    /// Struct for Cookie interface with given user data
+    struct Cookie
+    {
+        extern(C) int function(void* user_data, char*, int) readfn;
+        extern(C) int function(void* user_data, const char*, int) writefn;
+        extern(C) fpos_t function(void* user_data, fpos_t, int) seekfn;
+        extern(C) int function(void* user_data) closefn;
+        const void* user_data;
+    }
+
+    /// Type of file obviously
+    Type type;
+
+    /// mode of File
+    int mode;
+    /// File error
+    int error;
+    /// End-of-file indicator
+    bool eof;
+    /// Preallocated File struct (do not issue free)
+    bool prealloc;
+
+    /// Type-dependent fields:
+    union {
+        int raw_fd;
+        Mem memory;
+        Cookie cookie;
+    }
+};
+
+import nanoc.std.stdio.file.dynamic_memory;
+import nanoc.std.stdio.file.cookie;
+import nanoc.std.stdio.file.raw_fd;
+import nanoc.std.stdio.file.memory;
+
+import std.traits;
+import std.meta;
+
+/// Close stream
+int nanoclose(FILE* f)
+{
+    static foreach (x; EnumMembers!(File.Type))
+    {
+        if (f.type == x)
+        {
+            return FileInterface!(Alias!x)._fclose(f);
+        }
+    }
+    return EOF;
+}
+
+int nanoputc(int c, FILE* stream)
+{
+    static foreach (x; EnumMembers!(File.Type))
+    {
+        if (stream.type == x)
+        {
+            return FileInterface!(Alias!x)._fputc(c, stream);
+        }
+    }
+    return EOF;
+}
+
+size_t nanowrite(const void* ptr, size_t size, size_t nitems, FILE* stream)
+{
+    if (size == 0)
+    {
+        return 0;
+    }
+
+    byte* data = cast(byte*) ptr;
+    size_t n = nitems;
+    general: for (; n > 0; n--)
+    {
+        static foreach (x; EnumMembers!(File.Type))
+        {
+            if (stream.type == x)
+            {
+                int ret = FileInterface!(Alias!x)._write(stream, data, size);
+                if (ret == size)
+                {
+                    data += size;
+                }
+                else
+                {
+                    break general;
+                }
+            }
+        }
+    }
+
+    return nitems - n;
+}
+
+size_t nanoread(void* ptr, size_t size, size_t nitems, FILE* stream)
+{
+    if (size == 0)
+    {
+        return 0;
+    }
+
+    byte* data = cast(byte*) ptr;
+    size_t n = nitems;
+    general: for (; n > 0; n--)
+    {
+        static foreach (x; EnumMembers!(File.Type))
+        {
+            if (stream.type == x)
+            {
+                int ret = FileInterface!(Alias!x)._read(stream, data, size);
+                if (ret == size)
+                {
+                    data += size;
+                }
+                else
+                {
+                    break general;
+                }
+            }
+        }
+    }
+
+    return nitems - n;
+}
+
+/// Returns strlen(s) on success
+int nanoputs(const char* s, FILE* stream)
+{
+    import nanoc.std.string: strlen;
+    import nanoc.os;
+
+    size_t size = strlen(s);
+    long r = fwrite(cast(void*) s, 1, size, stream);
+
+    if (r == 0 && size != 0)
+    {
+        return EOF;
+    }
+
+    if (r >= 0)
+    {
+        return cast(int) r;
+    }
+    // inherits error or EOF possibly
+    return EOF;
+}
+
+int nanogetc(FILE *stream)
+{
+    static foreach (x; EnumMembers!(File.Type))
+    {
+        if (stream.type == x)
+        {
+            return FileInterface!(Alias!x)._fgetc(stream);
+        }
+    }
+    return EOF;
+}
+
+long nanotell(FILE* stream)
+{
+    static foreach (x; EnumMembers!(File.Type))
+    {
+        if (stream.type == x)
+        {
+            return FileInterface!(Alias!x)._seek(stream, 0, SEEK_CUR);
+        }
+    }
+    return -1;
+}
+
+int nanoseek(FILE *stream, long offset, int whence)
+{
+    static foreach (x; EnumMembers!(File.Type))
+    {
+        if (stream.type == x)
+        {
+            return FileInterface!(Alias!x)._seek(stream, offset, whence) == -1 ? -1 : 0;
+        }
+    }
+    return -1;
+}
+
+int nanoerror(FILE* stream)
+{
+    return stream.error;
+}
+
+void nanoclearerr(FILE *stream)
+{
+    stream.error = 0;
+    stream.eof = false;
+}
+
+void nanorewind(FILE *stream)
+{
+    nanoclearerr(stream);
+    nanoseek(stream, 0, SEEK_SET);
+}
+
+int nanoeof(FILE* stream)
+{
+    return stream.eof;
+}
--- source/std/stdio/file/package.d
+++ source/std/stdio/file/package.d
@@ -3,74 +3,12 @@ module nanoc.std.stdio.file;
 public import nanoc.std.stdio.file.dynamic_memory: open_memstream;
 public import nanoc.std.stdio.file.memory: fmemopen;
 public import nanoc.std.stdio.file.raw_fd: fopen;
-import nanoc.std.stdio.file.dynamic_memory;
-import nanoc.std.stdio.file.cookie;
-import nanoc.std.stdio.file.raw_fd;
-import nanoc.std.stdio.file.memory;
+public import nanoc.std.stdio.file.core: FILE;
 import nanoc.std.stdio.common;
-
-alias fpos_t = long;
-
-/// Internal struct for File
-struct FILE {
-    /// Define implementation of File template interface (template FileInterface(alias A))
-    enum Type {
-        OS = 1,
-        MEMORY_STREAM = 2,
-        DYNAMIC_MEMORY_STREAM = 3,
-        COOKIE = 4
-    }
-
-    /// Struct for memory streams
-    struct Mem {
-        /// pointer to buffer
-        void* data_ptr;
-        /// size of buffer
-        size_t size;
-        /// ftell
-        long offset;
-        /// indicates callee must free data_ptr
-        bool callee_free;
-
-        /// Only dynamic memory stream: for updating user point
-        void** dynamic_data;
-        /// Only dynamic memory stream: for updating user size
-        size_t* dynamic_size;
-    }
-
-    /// Struct for Cookie interface with given user data
-    struct Cookie
-    {
-        extern(C) int function(void* user_data, char*, int) readfn;
-        extern(C) int function(void* user_data, const char*, int) writefn;
-        extern(C) fpos_t function(void* user_data, fpos_t, int) seekfn;
-        extern(C) int function(void* user_data) closefn;
-        const void* user_data;
-    }
-
-    /// Type of file obviously
-    Type type;
-
-    // Generic fields, epilogue:
-    /// mode of File
-    int mode;
-    /// File error
-    int error;
-    /// End-of-file indicator
-    bool eof;
-    /// Preallocated File struct (do not issue free)
-    bool prealloc;
-
-    /// Type-dependent fields:
-    union {
-        int raw_fd;
-        Mem memory;
-        Cookie cookie;
-    }
-};
 
 /// Adorable alias
 alias File = FILE;
+alias fpos_t = long;
 
 __gshared File fstdin = {type: File.Type.OS, raw_fd: STDIN_FILENO, prealloc: true};
 __gshared File fstdout = {type: File.Type.OS, raw_fd: STDOUT_FILENO, prealloc: true};
@@ -94,147 +32,38 @@ File* checkStdHandler(File* f)
     return f;
 }
 
+import nanoc.std.stdio.file.core;
+
 /// Close stream
 extern (C) int fclose(FILE* f)
 {
-    f = checkStdHandler(f);
-    import std.traits;
-    import std.meta;
-
-    static foreach (x; EnumMembers!(File.Type))
-    {
-        if (f.type == x)
-        {
-            return FileInterface!(Alias!x)._fclose(f);
-        }
-    }
-    return EOF;
+    return nanoclose(checkStdHandler(f));
 }
 
 extern (C) int fputc(int c, FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    import std.traits;
-    import std.meta;
-
-    static foreach (x; EnumMembers!(File.Type))
-    {
-        if (stream.type == x)
-        {
-            return FileInterface!(Alias!x)._fputc(c, stream);
-        }
-    }
-    return EOF;
+    return nanoputc(c, checkStdHandler(stream));
 }
 
 extern (C) size_t fwrite(const void* ptr, size_t size, size_t nitems, FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    if (size == 0)
-    {
-        return 0;
-    }
-
-    size_t n = nitems;
-    import std.traits;
-    import std.meta;
-
-    byte* data = cast(byte*) ptr;
-    general: for (; n > 0; n--)
-    {
-        static foreach (x; EnumMembers!(File.Type))
-        {
-            if (stream.type == x)
-            {
-                int ret = FileInterface!(Alias!x)._write(stream, data, size);
-                if (ret == size)
-                {
-                    data += size;
-                }
-                else
-                {
-                    break general;
-                }
-            }
-        }
-    }
-
-    return nitems - n;
+    return nanowrite(ptr, size, nitems, checkStdHandler(stream));
 }
 
 extern (C) size_t fread(void* ptr, size_t size, size_t nitems, FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    if (size == 0)
-    {
-        return 0;
-    }
-
-    size_t n = nitems;
-    import std.traits;
-    import std.meta;
-
-    byte* data = cast(byte*) ptr;
-    general: for (; n > 0; n--)
-    {
-        static foreach (x; EnumMembers!(File.Type))
-        {
-            if (stream.type == x)
-            {
-                int ret = FileInterface!(Alias!x)._read(stream, data, size);
-                if (ret == size)
-                {
-                    data += size;
-                }
-                else
-                {
-                    break general;
-                }
-            }
-        }
-    }
-
-    return nitems - n;
+    return nanoread(ptr, size, nitems, checkStdHandler(stream));
 }
 
 /// Returns strlen(s) on success
 extern (C) int fputs(const char* s, FILE* stream)
 {
-    stream = checkStdHandler(stream);
-
-    import nanoc.std.string: strlen;
-    import nanoc.os;
-
-    size_t size = strlen(s);
-    long r = fwrite(cast(void*) s, 1, size, stream);
-
-    if (r == 0 && size != 0)
-    {
-        return EOF;
-    }
-
-    if (r >= 0)
-    {
-        return cast(int) r;
-    }
-    // inherits error or EOF possibly
-    return EOF;
+    return nanoputs(s, checkStdHandler(stream));
 }
 
 extern (C) int fgetc(FILE *stream)
 {
-    stream = checkStdHandler(stream);
-    import std.traits;
-    import std.meta;
-
-    static foreach (x; EnumMembers!(File.Type))
-    {
-        if (stream.type == x)
-        {
-            return FileInterface!(Alias!x)._fgetc(stream);
-        }
-    }
-    return EOF;
+    return nanogetc(checkStdHandler(stream));
 }
 
 extern(C) int remove(const char* pathname)
@@ -243,20 +72,9 @@ extern(C) int remove(const char* pathname)
     return unlink(pathname);
 }
 
-extern(C) long ftell(FILE* stream)
+extern(C) long nanotell(FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    import std.traits;
-    import std.meta;
-
-    static foreach (x; EnumMembers!(File.Type))
-    {
-        if (stream.type == x)
-        {
-            return FileInterface!(Alias!x)._seek(stream, 0, SEEK_CUR);
-        }
-    }
-    return -1;
+    return nanotell(checkStdHandler(stream));
 }
 
 enum SEEK_CUR = 1;
@@ -265,42 +83,25 @@ enum SEEK_SET = 0;
 
 extern(C) int fseek(FILE *stream, long offset, int whence)
 {
-    stream = checkStdHandler(stream);
-    import std.traits;
-    import std.meta;
-
-    static foreach (x; EnumMembers!(File.Type))
-    {
-        if (stream.type == x)
-        {
-            return FileInterface!(Alias!x)._seek(stream, offset, whence) == -1 ? -1 : 0;
-        }
-    }
-    return -1;
+    return nanoseek(checkStdHandler(stream), offset, whence);
 }
 
 extern (C) int ferror(FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    return stream.error;
+    return nanoerror(checkStdHandler(stream));
 }
 
 extern (C) void clearerr(FILE *stream)
 {
-    stream = checkStdHandler(stream);
-    stream.error = 0;
-    stream.eof = false;
+    nanoclearerr(checkStdHandler(stream));
 }
 
 extern (C) void rewind(FILE *stream)
 {
-    stream = checkStdHandler(stream);
-    clearerr(stream);
-    fseek(stream, 0, SEEK_SET);
+    nanorewind(checkStdHandler(stream));
 }
 
 extern (C) int feof(FILE* stream)
 {
-    stream = checkStdHandler(stream);
-    return stream.eof;
+    return nanoeof(checkStdHandler(stream));
 }
