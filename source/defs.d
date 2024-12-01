5db55b9ea70ceb3c5ce53269b9f8101cd20837d6
Yury Moskov <aerobus.778@gmail.com>
2024-12-01 04:19:37 +0300

    meta: add noreturn declaration

--- source/defs.d
+++ source/defs.d
@@ -1,5 +1,12 @@
 module nanoc.defs;
 
 import nanoc.meta: SetKey, Nake;
+import std.meta;
 
+@SetKey("noreturn") @Nake enum _noreturn = "[[ noreturn ]] void";
 @SetKey("ulong") @Nake enum _ulong = "unsigned long";
+@SetKey("ubyte") @Nake enum _ubyte = "unsigned char";
+@SetKey("byte") @Nake enum _byte = "char";
+
+struct FILE {}
+
