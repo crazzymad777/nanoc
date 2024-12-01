94260700ce3be3888d543b6558efb362bf802ead
Yury Moskov <aerobus.778@gmail.com>
2024-12-01 04:44:26 +0300

    meta: add some types

--- source/defs.d
+++ source/defs.d
@@ -3,7 +3,13 @@ module nanoc.defs;
 import nanoc.meta: SetKey, Nake;
 import std.meta;
 
+alias Includes = AliasSeq!("uchar.h");
+
 @SetKey("noreturn") @Nake enum _noreturn = "[[ noreturn ]] void";
+@SetKey("dchar") @Nake enum _dchar = "char32_t";
+@SetKey("wchar") @Nake enum _wchar = "char16_t";
+@SetKey("ushort") @Nake enum _ushort = "unsigned short";
+@SetKey("uint") @Nake enum _uint = "unsigned";
 @SetKey("ulong") @Nake enum _ulong = "unsigned long";
 @SetKey("ubyte") @Nake enum _ubyte = "unsigned char";
 @SetKey("byte") @Nake enum _byte = "char";
